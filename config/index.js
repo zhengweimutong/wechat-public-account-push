ad9824feac1229ab7a231934c649b02a6ec1d545
Star <113099388+zhengwei196615@users.noreply.github.com>
2022-09-08 17:41:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,34 +18,34 @@ export const config = {
    * [
    *  {
    *    // 想要发送的人的名字
-   *    name: "平平小仙女的人间使者",
+   *    name: "养猪大户",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "oWbBN5lbLJbD6E93xnUJWhZ98rjU",
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "PnO-uiJH5uTzTqYBf-oPMhZCXnTRDwOzGuuMT2Y_MrM",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
+   *    openUrl: "https://e.xiaohongshu.com/home?sourceId=7&gt_ki=449807635834&gt_ui=39609550&gt_pi=202508642&gt_uni=7115145289&gt_ci=63534770004&gt_mt=1&gt_dt=0&gt_of=nm&gt_cpi=0&gt_kw=%E5%B0%8F%E7%BA%A2%E4%B9%A6&utm_source=baidu_sem&utm_campaign=e_xiaohongshu&bd_vid=11587720581150155848"
    *   }, 
    * ]
    */
   USERS: [
     {
       // 想要发送的人的名字
-      name: "平平小仙女",
+      name: "养猪大户",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oWbBN5iabZl1v--MyeyWPXRU8yBw",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "PnO-uiJH5uTzTqYBf-oPMhZCXnTRDwOzGuuMT2Y_MrM",
       // 所在省份
-      province: "陕西",
+      province: "广东",
       // 所在城市
-      city: "杨凌",
+      city: "珠海",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://s.weibo.com/top/summary/"
+      openUrl: "https://e.xiaohongshu.com/home?sourceId=7&gt_ki=449807635834&gt_ui=39609550&gt_pi=202508642&gt_uni=7115145289&gt_ci=63534770004&gt_mt=1&gt_dt=0&gt_of=nm&gt_cpi=0&gt_kw=%E5%B0%8F%E7%BA%A2%E4%B9%A6&utm_source=baidu_sem&utm_campaign=e_xiaohongshu&bd_vid=11587720581150155848"
     }, 
     {
-      name: "平平小仙女的人间使者", 
-      id: "oWbBN5lbLJbD6E93xnUJWhZ98rjU",
+      name: "放牛小贝", 
+      id: "oloJU6CWWL1ddjgp0tq3IOAlRuFA",
       useTemplateId: "PnO-uiJH5uTzTqYBf-oPMhZCXnTRDwOzGuuMT2Y_MrM",
       province: "陕西",
       city: "杨凌",
@@ -80,9 +80,9 @@ export const config = {
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: "平平小仙女的人间使者",
+      name: "养猪大户",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "JXiyY1XkQKrjqcSKgMXYsxoF_UGOa22UthCAglSOSIg",
+      id: "oloJU6Cm5-4CqhYey4aaPz1gjrUU",
     }, 
   ],
     
@@ -106,7 +106,7 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "平平小仙女", "year": "1997", "date": "08-08"},
+    {"type": "生日", "name": "放牛小贝", "year": "1997", "date": "11-15"},
   ],
 
   /**
@@ -124,14 +124,10 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2021-10-31"},
-    // 结婚纪念日
-    // {"keyword": "marry_day", date: "2027-06-06"},
-    // 退伍日, 不用可以删掉
-    //{"keyword": "ex_day", date: "2022-08-31"},
-    // sakana日
-    // {"keyword": "sakana_day", date: "2022-01-06"},
-    // ... 
+    {"keyword": "love_day", date: "2022-03-12"},
+   
+    
+   
   ],
 
   /** 插槽 */
@@ -145,10 +141,10 @@ export const config = {
     {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
     // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
-      "因为太喜欢你，所以看谁都像是情敌。",
-      "申请成为你爱里的永久居民。",
-      "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-      "遇见你，就好像捡到了100斤的运气",
+      "我是你最大的底气。",
+      "新的一天，爱你。",
+      "今天也要努力啊！",
+     
     ]},
   ],
 
