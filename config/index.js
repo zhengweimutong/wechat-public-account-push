fa4b5100e96d4ef0abeb2e0e82631fbad4733709
Morgen-G <96287286+Morgen-G@users.noreply.github.com>
2022-09-08 16:23:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ export const config = {
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "oWbBN5lbLJbD6E93xnUJWhZ98rjU",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "SJcZu6QbtTqcy9I1iV4yRjDWjjerIFVMp4GP_ho6IgY",
+   *    useTemplateId: "l3NhE_7T8JEAigsv5cgmFQ7M2isqn1kTGQI_Y7kRB74",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
@@ -35,13 +35,13 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oWbBN5lbLJbD6E93xnUJWhZ98rjU",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "SJcZu6QbtTqcy9I1iV4yRjDWjjerIFVMp4GP_ho6IgY",
+      useTemplateId: "l3NhE_7T8JEAigsv5cgmFQ7M2isqn1kTGQI_Y7kRB74",
       // 所在省份
       province: "陕西",
       // 所在城市
       city: "咸阳",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://s.weibo.com/top/summary/"
     }, 
     {
       name: "老婆1", 
@@ -82,7 +82,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "	JXiyY1XkQKrjqcSKgMXYsxoF_UGOa22UthCAglSOSIg",
     }, 
   ],
     
